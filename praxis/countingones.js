5800baa2de6f3894eec520ab3797a3592f5a5690
Christian Delahousse <christian.delahousse@gmail.com>
2012-07-27 20:45:47 -0400

    adsf

---
+++ praxis/countingones.js
@@ -0,0 +1,42 @@
+﻿/*
+ * Counting Ones - 2012/06/15
+ *
+ * Today’s exercise is a fun math problem:
+ *
+ * Consider a function f that takes a positive integer n and returns the number of 1s
+ * in the decimal representation of all the integers from 0 to n, inclusive. For
+ * example, f(13) = 6, for the numbers 1, 10, 11 (twice, for two 1s), 12 and 13.
+ * Notice that f(1) = 1. After 1, what is the next largest n for which f(n) = n?
+ */
+
+function enumRange(start, end) {
+	var range = new Array(end - start + 1);
+
+	for (;start <= end; end--) {
+		range[end-start] = end;
+	}
+	return range;
+}
+
+
+function countOnes(num) {
+	num = "" + num;
+	return (num.match(/1/g)||[]).length;
+}
+
+//console.log(countOnes(enumRange(0,13)))
+console.log(countOnes(111));
+
+function largest() {
+	var i = 1;
+
+	var sum = 1;
+
+	do {
+		i++;
+		sum += countOnes(i);
+
+	} while (i !== sum) ;
+	return i;
+}
+console.log(largest());
---
+++ praxis/min-stack.js
@@ -0,0 +1,76 @@
+﻿//if JS running in Rhino
+if (typeof Packages === "object" && String(Packages) === "[JavaPackage ]") {
+	load('init.js');
+}
+/* 
+ * Christian Delahousse
+ * Programming Praxis
+ * 2012/07/27  
+ * Min stack
+ * http://programmingpraxis.com/2012/07/27/min-stack/
+ *
+ * Create a stack that does push and pop operations in O(1), but
+ * also finds (but doesn't remove) the minimum element, also in 
+ * O(1) time;
+ *
+*/
+
+
+
+function MinStack() {
+	var pushPopStack= []
+		, minimumsStack = []
+		, n = 0 //Num of elems in structure
+		, m = 0;// num of elems in min stack
+
+
+	this.push = function (elem) {
+
+		//If num of elems is zero, push to both stacks. 1st elem
+		//is also a minimum
+		if (n === 0) {
+			minimumsStack.push(elem);
+			m++;
+		}
+		//If new elem is lower than current min, we push a new min
+		else if (elem < minimumsStack[m-1]) {
+			minimumsStack.push(elem);
+			m++;
+		}
+
+		pushPopStack.push(elem);
+		n++;
+
+	};
+
+	
+	this.pop = function () {
+
+		if (n === 0) {
+			throw "No elems";
+		}
+
+		var elem = pushPopStack.pop();
+		n--;
+
+		//If elem matches pop of min stack, pop that one
+		//off too
+		if (elem === minimumsStack[m-1]) {
+			m--;
+			minimumsStack.pop();
+		}
+		return elem;
+	};
+
+	//Return minimun
+	this.min = function () {
+		return minimumsStack[m-1];
+	};
+
+	this.toString = function () {
+		var str = pushPopStack + " | " + minimumsStack;
+		return str;
+	};
+}
+
+
